2208c8834b97926ab2f4c2b683e3f651af77798c
Rich <applerich0306@gmail.com>
2023-07-13 22:01:26 +0300

    modify hash crpty feature into users

--- queries/functions/users/get_user_by_id.sql
+++ queries/functions/users/get_user_by_id.sql
@@ -1,3 +1,18 @@
-SELECT id, first_name, last_name, email, password, role
-FROM users
-WHERE id = 1;
+CREATE OR REPLACE FUNCTION get_user_by_id(user_id integer)
+  RETURNS json AS
+$$
+DECLARE
+  result json;
+BEGIN
+  SELECT json_agg(row_to_json(t))
+    INTO result
+    FROM (
+      SELECT id, first_name, last_name, email, password, role
+        FROM users
+       WHERE id = user_id
+    ) t;
+  
+  RETURN result;
+END;
+$$
+LANGUAGE plpgsql;
